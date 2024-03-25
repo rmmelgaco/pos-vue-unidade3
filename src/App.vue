8b986185be8ab9181827cf668bc10f211a50de91
melgacorm <rmmelgaco@gmail.com>
2024-03-25 11:39:46 -0300

    Criando subrotas para jogos e classificação do campeonato

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ const menus = [
 <template>
   <header>
     <div class='wrapper'>
-      <nav>
-        <RouterLink v-for='(menu, indice) in menus' :key='`menu${indice}`' :to='menu.rota'>
+      <nav class='tab_menu'>
+        <RouterLink class='tab_item' v-for='(menu, indice) in menus' :key='`menu${indice}`' :to='menu.rota'>
           {{menu.label}}
         </RouterLink>
       </nav>
@@ -20,4 +20,20 @@ const menus = [
 </template>
 
 <style scoped>
+.tab_menu {
+  margin: 5px;
+  display: flex;
+  justify-content: space-evenly;
+  border-bottom: 1px solid #52e7d6;
+}
+.tab_item {
+  padding: 10px;
+  color: #15295d;
+  flex: 1;
+  text-align: center;
+  font-size: larger;
+  font-weight: bolder;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
 </style>
--- src/components/JogosCampeonato.vue
+++ src/components/JogosCampeonato.vue
@@ -1,6 +1,73 @@
 <script setup>
+import {ref} from 'vue';
+
+const {codigoCampeonato} = defineProps(['codigoCampeonato'])
+const jogosCampeonato = ref(null)
+const mensagem = ref('')
+
+function recuperaTime(dados, codigoTime) {
+  return dados.times.find(time => time.codigo === codigoTime)
+}
+
+function preencheDadosJogo(jogoCampeonato, dados) {
+  const timeMandante = recuperaTime(dados, jogoCampeonato.codigoTimeMandante)
+  const timeVisitante = recuperaTime(dados, jogoCampeonato.codigoTimeVisitante)
+  return {
+    ...jogoCampeonato,
+    timeMandante,
+    timeVisitante
+  }
+}
+
+fetch('../src/assets/dados.json')
+    .then(dados => dados.json())
+    .then(dados => jogosCampeonato.value =
+        dados.jogos
+            .filter(jogo => jogo.codigoCampeonato === +codigoCampeonato)
+            .map(jogo => preencheDadosJogo(jogo, dados))
+    )
+    .catch(error => mensagem.value = error)
 </script>
 
 <template>
   <h5>Jogos</h5>
+  <div
+      class='card'
+      v-for='(jogoCampeonato, indice) in jogosCampeonato'>
+    <span class='timeMandante'>{{ jogoCampeonato.timeMandante.nome }}</span>
+    <span class='gols'>{{jogoCampeonato.resultado.golsMandante}}</span>
+    <span class='x'>X</span>
+    <span class='gols'>{{jogoCampeonato.resultado.golsVisitante}}</span>
+    <span class='timeVisitante'>{{ jogoCampeonato.timeVisitante.nome }}</span>
+
+  </div>
 </template>
+<style scoped>
+.card {
+  display: inline-flex;
+  flex-direction: row;
+  cursor: pointer;
+  margin: 10px;
+  padding: 10px;
+  min-width: 170px;
+  box-shadow: 1px 1px 5px lightgray;
+}
+
+.timeMandante {
+  padding: 10px;
+}
+
+.timeVisitante {
+  padding: 10px;
+}
+
+.gols {
+  padding: 10px;
+  font-weight: bolder;
+  border: 1px solid;
+}
+
+.x {
+  padding: 10px;
+}
+</style>
